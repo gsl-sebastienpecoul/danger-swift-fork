229e31d2c30437f825cd360c7b2f9838a782140a
Sébastien pécoul <sebastien.pecoul-ext@groupeseloger.com>
2024-09-27 12:06:38 +0200

    fix: Get rid of Swift format

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 // Version number can be found in Source/Danger/Danger.swift
 
 // switch to false when release
-let isDevelop = true
+let isDevelop = false
 
 let swiftLint: Package.Dependency = {
     #if compiler(>=5.9)
@@ -18,34 +18,43 @@ let swiftLint: Package.Dependency = {
 let devProducts: [Product] = isDevelop
     ? [
         .library(name: "DangerDeps", type: .dynamic, targets: ["Danger-Swift"])
-    ] : []
+    ]
+    : []
 let devDependencies: [Package.Dependency] = isDevelop
     ? [
         .package(url: "https://github.com/shibapm/Komondor", from: "1.1.4"),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.54.0"),
+        // .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.54.0"),
         swiftLint,
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.17.0"),
         .package(url: "https://github.com/shibapm/Rocket", from: "1.3.0"),
-    ] : []
+    ]
+    : []
 let devTargets: [Target] = isDevelop
     ? [
-        .testTarget(name: "DangerTests",
-                    dependencies: [
-                        "Danger",
-                        "DangerFixtures",
-                        .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
-                    ]),
-        .testTarget(name: "RunnerLibTests",
-                    dependencies: [
-                        "RunnerLib",
-                        .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
-        ], exclude: ["__Snapshots__"]),
-        .testTarget(name: "DangerDependenciesResolverTests",
-                    dependencies: [
-                        "DangerDependenciesResolver",
-                        .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
-                    ],
-                    exclude: ["__Snapshots__"]),
+        .testTarget(
+            name: "DangerTests",
+            dependencies: [
+                "Danger",
+                "DangerFixtures",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+            ]
+        ),
+        .testTarget(
+            name: "RunnerLibTests",
+            dependencies: [
+                "RunnerLib",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+            ],
+            exclude: ["__Snapshots__"]
+        ),
+        .testTarget(
+            name: "DangerDependenciesResolverTests",
+            dependencies: [
+                "DangerDependenciesResolver",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+            ],
+            exclude: ["__Snapshots__"]
+        ),
     ]
     : []
 
@@ -54,12 +63,16 @@ let package = Package(
     products: [
         .library(name: "Danger", targets: ["Danger"]),
         .library(name: "DangerFixtures", targets: ["DangerFixtures"]),
-        .executable(name: "danger-swift", targets: ["Runner"]),
+        .executable(name: "danger-swift", targets: ["Runner"])
     ] + devProducts,
     dependencies: [
         .package(url: "https://github.com/shibapm/Logger", from: "0.1.0"),
         .package(url: "https://github.com/mxcl/Version", from: "2.0.1"),
-        .package(url: "https://github.com/nerdishbynature/octokit.swift", from: "0.13.0"),
+        .package(
+            url: "https://github.com/gsl-sebastienpecoul/fork-octokit.swift",
+            branch: "chore/remove_swift_format"
+        )
+
     ] + devDependencies,
     targets: [
         .target(name: "Danger-Swift", dependencies: ["Danger"]),
@@ -68,50 +81,50 @@ let package = Package(
         .target(
             name: "Danger",
             dependencies: [
-                .product(name: "OctoKit", package: "octokit.swift"),
+                .product(name: "OctoKit", package: "fork-octokit.swift"),
                 "Logger",
                 "DangerShellExecutor"
             ]
         ),
         .target(name: "RunnerLib", dependencies: ["Logger", "DangerShellExecutor", "Version"]),
         .executableTarget(name: "Runner", dependencies: ["RunnerLib", "Logger", "DangerDependenciesResolver"]),
-        .target(name: "DangerFixtures", dependencies: ["Danger"]),
+        .target(name: "DangerFixtures", dependencies: ["Danger"])
     ] + devTargets
 )
 
 #if canImport(PackageConfig)
-    import PackageConfig
+import PackageConfig
 
-    let config = PackageConfiguration([
-        "komondor": [
-            "pre-push": "swift test",
-            "pre-commit": [
-                "swift test",
-                "swift test --generate-linuxmain",
-                "swift run swiftformat .",
-                "swift run swiftlint autocorrect --path Sources/",
-                "git add .",
-            ],
+let config = PackageConfiguration([
+    "komondor": [
+        "pre-push": "swift test",
+        "pre-commit": [
+            "swift test",
+            "swift test --generate-linuxmain",
+            "swift run swiftformat .",
+            "swift run swiftlint autocorrect --path Sources/",
+            "git add ."
+        ]
+    ],
+    "rocket": [
+        "pre_release_checks": [
+            "clean_git"
         ],
-        "rocket": [
-            "pre_release_checks": [
-                "clean_git",
-            ],
-            "steps": [
-                "Scripts/update_makefile.sh",
-                "Scripts/update_danger_version.sh",
-                "Scripts/update_changelog.sh",
-                "Scripts/change_is_develop.sh false",
-                "git_add",
-                "commit",
-                "tag",
-                "push",
-                "Scripts/change_is_develop.sh true",
-                "git_add",
-                ["commit": ["message": "Enable dev depdendencies"]],
-                "push",
-                "Scripts/create_homebrew_tap.sh",
-            ],
-        ],
-    ]).write()
+        "steps": [
+            "Scripts/update_makefile.sh",
+            "Scripts/update_danger_version.sh",
+            "Scripts/update_changelog.sh",
+            "Scripts/change_is_develop.sh false",
+            "git_add",
+            "commit",
+            "tag",
+            "push",
+            "Scripts/change_is_develop.sh true",
+            "git_add",
+            ["commit": ["message": "Enable dev depdendencies"]],
+            "push",
+            "Scripts/create_homebrew_tap.sh"
+        ]
+    ]
+]).write()
 #endif
